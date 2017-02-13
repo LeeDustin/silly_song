942480716de44881c43b54217ffafbbbdcf52dd4
Marwan Alani <marwan@alani.ca>
2017-02-12 20:57:04 -0500

    Added helper functions & templates to main view controller

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -25,10 +25,43 @@ class ViewController: UIViewController {
     }
 
     @IBAction func reset(_ sender: Any) {
-        
+        nameField.text = ""
+        lyricsView.text = ""
     }
 
     @IBAction func displayLyrics(_ sender: Any) {
     }
 }
+
+// Helper Functions
+func shortNameFromName(name: String) -> String {
+    let input = name.lowercased()
+    var output = ""
+    let consonants = "bcdfghjklmnpqrstvwxyz".characters
+    var firstNonConsonantFound = false
+    for char in input.characters {
+        if (firstNonConsonantFound) {
+            output.append(char)
+        } else {
+            if (!consonants.contains(char)) {
+                output.append(char)
+                firstNonConsonantFound = true
+            }
+        }
+    }
+    return output
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    var output = lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+    output = output.replacingOccurrences(of: "<SHORT_NAME>", with: shortNameFromName(name: fullName))
+    return output
+}
+
+// Templates Defenitions
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
 
